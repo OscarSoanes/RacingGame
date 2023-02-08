50778c399d389425be7c8a314fde539cbd282e95
Oscar Soanes <oscarsoanes@gmail.com>
2023-02-08 13:53:40 +0000

    tidy up

--- src/input.js
+++ src/input.js
@@ -7,48 +7,5 @@ export default class InputHandler {
     window.addEventListener("keyup", (ev) => {
       car.keys[ev.key] = false;
     });
-
-    // document.addEventListener("keydown", (event) => {
-    //   if (event.key == "ArrowUp") {
-    //     car.acceleration.x = Math.cos(car.rotation) * 0.05;
-    //     car.acceleration.y = Math.sin(car.rotation) * 0.05;
-    //   }
-
-    //   // console.log(car.acceleration);
-
-    //   car.updatePosition();
-    //   car.moving = true;
-    // });
-
-    // // document.addEventListener('keydown', (event) => {
-    // //     if (event.key == "ArrowDown") {
-    // //         car.moveDown();
-    // //     }
-    // // });
-
-    // document.addEventListener("keydown", (event) => {
-    //   if (event.key == "ArrowLeft") {
-    //     car.rotation += -0.07;
-    //     car.updatePosition();
-    //     car.moving = true;
-    //   }
-    // });
-
-    // document.addEventListener("keydown", (event) => {
-    //   if (event.key == "ArrowRight") {
-    //     car.rotation -= 0.07;
-    //     car.updatePosition();
-    //     car.moving = true;
-    //   }
-    // });
-
-    // document.addEventListener("keyup", (event) => {
-    //   switch (event.key) {
-    //     case "ArrowUp":
-    //       car.acceleration.x = 0;
-    //       car.acceleration.y = 0;
-    //       break;
-    //   }
-    // });
   }
 }
