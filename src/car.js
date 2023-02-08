6bae589cf16f9675cc6d1970b1cf271f1b355bd9
Oscar Soanes <oscarsoanes@gmail.com>
2023-02-08 12:24:29 +0000

    update

--- src/car.js
+++ src/car.js
@@ -1,65 +1,66 @@
 export default class Car {
-    constructor(game) {
-        this.gameWidth = game.gameWidth;
-        this.width = 30;
-        this.height = 50;
+  constructor(game) {
+    this.gameWidth = game.gameWidth;
+    this.width = 50;
+    this.height = 30;
 
-        this.position = {
-            x: game.gameWidth / 2 - this.width / 2,
-            y: game.gameHeight / 2 - this.height / 2,
-        };
+    this.position = {
+      x: game.gameWidth / 2 - this.width / 2,
+      y: game.gameHeight / 2 - this.height / 2,
+    };
 
-        this.velocity = {
-            x: 0,
-            y: 0,
-        };
+    this.velocity = {
+      x: 0,
+      y: 0,
+    };
 
-        this.acceleration = {
-            x: 0,
-            y: 0,
-        }
+    this.acceleration = {
+      x: 0,
+      y: 0,
+    };
 
-        this.friction = 0.95;
+    this.friction = 0.95;
 
-        this.rotation = 0;
+    this.rotation = 0;
+
+    this.moving = false;
+  }
+
+  updatePosition() {
+    // update velocity
+    this.velocity.x += this.acceleration.x;
+    this.velocity.y += this.acceleration.y;
+
+    // friction
+    this.velocity.x *= this.friction;
+    this.velocity.y *= this.friction;
+
+    // update position
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+  }
+
+  update(_deltaTime) {
+    this.updatePosition();
+
+    if (this.moving === true) {
+      this.position.x += this.velocity.x;
+      this.position.y += this.velocity.y;
     }
 
-    updatePosition(obj) {
-        // update velocity
-        this.velocity.x += this.acceleration.x;
-        this.velocity.y += this.acceleration.y;
-
-        // friction
-        this.velocity.x *= this.friction;
-        this.velocity.y *= this.friction;
-
-        // update position
-        this.position.x += this.velocity.x;
-        this.position.y += this.velocity.y;
-
+    if (this.position.x < 0) this.position.x = 0;
+    if (this.position.x + this.width > this.gameWidth) {
+      this.position.x = this.gameWidth - this.width;
     }
+  }
 
-    update(deltaTime) {
-        this.position.x += this.velocity.x;
-        this.position.y += this.velocity.y;
-
-        this.updatePosition();
-
-        // console.log(`${this.velocity.x}, ${this.velocity.y}`);
-
-        // console.log(`${this.position.x} ${this.position.y}`);
-        if (this.position.x < 0) this.position.x = 0;
-        if (this.position.x + this.width > this.gameWidth) {
-            this.position.x = this.gameWidth - this.width;
-        }
-    }
-
-    draw(ctx) {
-        ctx.save();
-        ctx.translate(this.position.x, this.position.y);
-        ctx.rotate(this.rotation);
-        ctx.fillStyle = 'black';
-        ctx.fillRect(-this.height / 2, this.width / 2, this.height, this.width);
-        ctx.restore();
-    }
+  draw(ctx) {
+    ctx.save();
+    ctx.translate(this.position.x, this.position.y);
+    ctx.rotate(this.rotation);
+    ctx.fillStyle = "black";
+    // ctx.fillRect(-this.height / 2, this.width / 2, this.height, this.width);
+    ctx.fillRect(-this.width / 2, -this.height / 2, this.width, this.height);
+    ctx.restore();
+  }
 }
--- src/input.js
+++ src/input.js
@@ -1,54 +1,46 @@
 export default class InputHandler {
-    constructor(car) {
-        document.addEventListener('keydown', (event) => {
-            if (event.key == "ArrowUp") {
-                car.acceleration.x = Math.cos(car.rotation) * 0.05;
-                car.acceleration.y = Math.sin(car.rotation) * 0.05;
-            } else {
-                car.acceleration.x = 0;
-                car.acceleration.y = 0;
-            }
+  constructor(car) {
+    document.addEventListener("keydown", (event) => {
+      if (event.key == "ArrowUp") {
+        car.acceleration.x = Math.cos(car.rotation) * 0.05;
+        car.acceleration.y = Math.sin(car.rotation) * 0.05;
+      }
 
-            // console.log(car.acceleration);
+      // console.log(car.acceleration);
 
-            car.updatePosition();
-        });
+      car.updatePosition();
+      car.moving = true;
+    });
 
-        // document.addEventListener('keydown', (event) => {
-        //     if (event.key == "ArrowDown") {
-        //         car.moveDown();
-        //     }
-        // });
+    // document.addEventListener('keydown', (event) => {
+    //     if (event.key == "ArrowDown") {
+    //         car.moveDown();
+    //     }
+    // });
 
-        document.addEventListener('keydown', (event) => {
-            if (event.key == "ArrowLeft") {
-                car.rotation += - 0.03;
-                car.updatePosition();
+    document.addEventListener("keydown", (event) => {
+      if (event.key == "ArrowLeft") {
+        car.rotation += -0.07;
+        car.updatePosition();
+        car.moving = true;
+      }
+    });
 
-            }
-        });
+    document.addEventListener("keydown", (event) => {
+      if (event.key == "ArrowRight") {
+        car.rotation -= 0.07;
+        car.updatePosition();
+        car.moving = true;
+      }
+    });
 
-        document.addEventListener('keydown', (event) => {
-            if (event.key == "ArrowRight") {
-                car.rotation -= 0.03;
-                car.updatePosition();
-            }
-        });
-
-
-
-        document.addEventListener('keyup', (event) => {
-            switch(event.key) {
-                case 'ArrowUp':
-                    break;
-                case 'ArrowDown':
-                    break;
-                case 'ArrowLeft':
-                    break;
-                case 'ArrowRight':
-                    break;
-            }
-        });
-    }
-            
+    document.addEventListener("keyup", (event) => {
+      switch (event.key) {
+        case "ArrowUp":
+          car.acceleration.x = 0;
+          car.acceleration.y = 0;
+          break;
+      }
+    });
+  }
 }
